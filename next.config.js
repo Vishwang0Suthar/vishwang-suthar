a3da04609df4a89f963570c6744b2feea161ddb8
vishwang suthar <100853098+Vishwang0Suthar@users.noreply.github.com>
2025-02-17 15:10:43 +0530

    more & more deployment errors resolved

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const { withStoreConfig } = require("./store-config")
+// const store = require("./store.config.json")
+
+module.exports = {
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+        // for webpack 5 use
+        // { and: [/\.(js|ts)x?$/] }
+      },
+
+      use: ['@svgr/webpack'],
     });
 
     return config;
@@ -20,5 +28,3 @@ const nextConfig = {
     ],
   },
 };
-
-export default nextConfig;
