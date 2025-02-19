c58c412ba908474dd8564d776170c4d9a5fe58b8
vishwang suthar <100853098+Vishwang0Suthar@users.noreply.github.com>
2025-02-19 18:02:58 +0530

    portfolio issue

--- next.config.js
+++ next.config.js
@@ -4,29 +4,15 @@
 
 module.exports = {
   webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ["@svgr/webpack"],
-      }
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    return config;
+    return config
   },
+
 
   reactStrictMode: true,
   images: {
